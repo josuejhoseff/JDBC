5cb494270e331e4fb30159b5b98dca2c2d6b1b08
Josue Jhoseff <josuejhosefff@gmail.com>
2023-01-13 16:13:14 -0300

    Transações

--- src/application/program.java
+++ src/application/program.java
@@ -14,26 +14,51 @@ import db.DbException;
 public class program {
 
 	public static void main(String[] args) {
+		
+		/*Transações devem ser:
+		 * Atomica - ou acontece tudo ou não acontece nada
+		 * Consistente
+		 * Isolada
+		 * Duravel
+		 * 
+		 * Ex: Transações bancarias
+		 */
+		
 
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
+		
 		try {
 			conn = DB.getConnection();
 	
-			st = conn.prepareStatement(
-					"DELETE FROM seller "
-					+ "WHERE "
-					+ "Id = ?");
+			conn.setAutoCommit(false);// (false) - Cada operaçao não está confirmada
 
-			st.setInt(1, 2);
+			st = conn.createStatement();
 			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 
-		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		} finally {
+			//int x = 1;
+			//if (x < 2) {
+			//	throw new SQLException("Fake error");
+			//}
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();// Confirmar a transação
+			
+			System.out.println("rows1 = " + rows1);
+			System.out.println("rows2 = " + rows2);
+		}
+		catch (SQLException e) {
+			try {
+				conn.rollback(); // Desfazer o que foi feito até o momento
+				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+			} 
+			catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
+		} 
+		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
