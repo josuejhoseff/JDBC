b42e69b72d1796327dc8653b4fc9960e4d6eb252
Josue Jhoseff <josuejhosefff@gmail.com>
2023-01-13 15:50:14 -0300

    Deletar dados

--- src/application/program.java
+++ src/application/program.java
@@ -17,25 +17,22 @@ public class program {
 
 		Connection conn = null;
 		PreparedStatement st = null;
-
 		try {
 			conn = DB.getConnection();
-
+	
 			st = conn.prepareStatement(
-					"UPDATE seller "  
-			+ "SET BaseSalary = BaseSalary + ? " 
-			+ "WHERE " 
-			+ "(DepartmentId = ?)");
+					"DELETE FROM seller "
+					+ "WHERE "
+					+ "Id = ?");
 
-			st.setString(1, "2000");
-			st.setString(2, "2");
-
+			st.setInt(1, 2);
+			
 			int rowsAffected = st.executeUpdate();
-
-			System.out.println("Done! rows affected: " + rowsAffected);
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
 
 		} catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
